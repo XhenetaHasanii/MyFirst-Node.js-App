5443610040fd111bf0198fd491820dafd0594dfa
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-11-02 12:25:23 +0100

    Added 'node mon' package

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log(" This is Nodejs Tutorial for Beginners");
+console.log("This is Nodejs Tutorial for Beginners");
+console.log("This is the first tutorial on nodejs");
+console.log("We will cover Nodejs with command line");
+console.log("hello again");
+
