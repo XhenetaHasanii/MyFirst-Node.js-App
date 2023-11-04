12005dca147406cb9dd1a82572dfc8fb04b3088f
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-11-04 18:57:49 +0100

    Node.js as a File Server

---
+++ files.js
@@ -0,0 +1,52 @@
+const fs=require('fs');
+
+// reading files
+fs.readFile('./docs/blog.txt',(err,data)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(data.toString());
+});
+console.log('last line');
+
+// writing files
+fs.writeFile('./docs/blog.txt','hello, world',()=>
+{
+    console.log('file was written');
+});
+
+// writing on another file
+fs.writeFile('./docs/blog2.txt','hello, again',()=>
+{
+    console.log('file was written');
+});
+
+// directories 
+if(!fs.existsSync('./assets')){
+    fs.mkdir('./assets',(err)=>{
+        if(err)
+        {
+            console.log(err);
+        }
+        console.log('folder created');
+    });}
+    else{
+        fs.rmdir('./assets',(err)=>{
+            if(err){
+                console.log(err);
+            }
+            console.log('folder deleted');
+        }
+        )
+    }
+   
+    // deleting files
+    if(fs.existsSync('./docs/deleteme.txt')){
+    fs.unlink('./docs/deleteme.txt',(err)=>{
+    if(err){
+        console.log(err);
+    }
+        console.log('File deleted');
+    })
+}
