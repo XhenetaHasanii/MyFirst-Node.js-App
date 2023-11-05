68e6dcb3315325f15b690c48959cb536b30757e2
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-11-05 17:28:56 +0100

    Returning html pages my response

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 // include HTTP module
-const http=require('http');
+const http=require('http')
 
 // create a server object
 const server =http.createServer((req,res)=>{
     console.log(req.url , req.method);
     // set header content type 
-    res.setHeader('Content-Type','text/html');
-    res.write('<head><link rel="stylesheet" href="#"</head>')
-    res.write('<p>hello, ninjas</p>');
-    res.end();
+   res.setHeader('Content-Type','text/html');
+   res.write('<p>hello, ninjas!</p>')
+   res.write('<head<link style="stylesheet",href="#"></head>')
 
-});  
+  //read from index.html file
+   const fs=require('fs');
+   fs.readFile('./views/index.html',(err,data)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        res.write(data);
+    }
+})});  
 server.listen(3000,'localhost',()=>{
     console.log('listening for request on port 3000');
 });
+
