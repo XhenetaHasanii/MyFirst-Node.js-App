997de954cb1f9bd6b57a49f82eb5710737f7624c
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-11-05 12:16:14 +0100

    Created a html response for my request

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 // include HTTP module
 const http=require('http');
 
-
 // create a server object
 const server =http.createServer((req,res)=>{
-    console.log('request made');
-});
+    console.log(req.url , req.method);
+    // set header content type 
+    res.setHeader('Content-Type','text/html');
+    res.write('<head><link rel="stylesheet" href="#"</head>')
+    res.write('<p>hello, ninjas</p?>');
+    res.write('<p>hello ,xheni</p>');
+    res.end();
 
+});  
 server.listen(3000,'localhost',()=>{
     console.log('listening for request on port 3000');
 });
