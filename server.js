6e325aff9ee9dd3ca9b47d1dc0dd72b21dbcf553
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-11-05 17:48:30 +0100

    Some updates in 'server.js' file

--- server.js
+++ server.js
@@ -2,21 +2,28 @@
 const http=require('http')
 
 // create a server object
-const server =http.createServer((req,res)=>{
+ const server =http.createServer((req,res)=>{
     console.log(req.url , req.method);
-    // set header content type 
-   res.setHeader('Content-Type','text/html');
-   res.write('<p>hello, ninjas!</p>')
-   res.write('<head<link style="stylesheet",href="#"></head>')
 
-  //read from index.html file
+//set header content-type
+   res.setHeader('Content-Type','text/html');
+   
+//res.setHeader('Content-Type','text/plain');
+//res.write('<p>hello, ninjas!</p>');
+//res.write('<head<link style="stylesheet",href="#"></head>');
+
+//include file system
    const fs=require('fs');
+
+ // send an html file
    fs.readFile('./views/index.html',(err,data)=>{
     if(err){
         console.log(err)
     }
     else{
         res.write(data);
+        res.end();
+        
     }
 })});  
 server.listen(3000,'localhost',()=>{
