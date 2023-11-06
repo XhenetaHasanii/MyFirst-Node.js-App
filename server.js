f0fabe99fe43abc4cf386ea484ebe98df72c49d6
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-11-06 08:20:42 +0100

    Used a switch statement to figure out what URL a user has visited

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const http=require('http');
  const server =http.createServer((req,res)=>{
     console.log(req.url , req.method);
 
+    let path='./views/';
+    switch(req.url){
+case '/':
+    path+='index.html';
+    break;
+case '/about':
+    path+='about.html';
+break;
+case '/404':
+    path+='404.html';
+    break;
+}
 //set header content-type
    res.setHeader('Content-Type','text/html');
    
@@ -16,7 +28,7 @@ const http=require('http');
    const fs=require('fs');
 
  // send an html file
-   fs.readFile('./views/index.html',(err,data)=>{
+   fs.readFile(path,(err,data)=>{
     if(err){
         console.log(err)
     }
@@ -28,3 +40,4 @@ const http=require('http');
 server.listen(3000,'localhost',()=>{
     console.log('listening for request on port 3000');
 });
+
