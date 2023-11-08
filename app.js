1b1a5b7c6c951e2390e9bca686853b17576f3269
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-11-08 10:34:23 +0100

    Some updates

--- app.js
+++ app.js
@@ -18,17 +18,13 @@ app.get('/about',(req,res)=>{
     res.sendFile('./views/about.html',{root:__dirname});
 });
 
-// redirects
+//redirects
 app.get('/about-us',(req,res)=>{
     res.redirect('/about');
 });
 
 // 404 page
-app.use((req,res)=>{
+app.use((res,res)=>{
     res.status(404).sendFile('./views/404.html',{root:__dirname});
 });
-
-
-
-
 
